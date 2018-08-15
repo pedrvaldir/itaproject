c5e1ea5a5b95cf3f2d2f9e5a7a6ec583adca6c8c
Valdeci Pedroso <valdecipti@gmail.com>
2018-08-14 23:30:49 -0300

    Changed the way that load the image of firebase storage

--- app/src/main/java/com/example/valdir/appitarare/ui/activities/AdvertisementActivity.java
+++ app/src/main/java/com/example/valdir/appitarare/ui/activities/AdvertisementActivity.java
@@ -33,7 +33,8 @@ public class AdvertisementActivity extends AppCompatActivity {
 
         final Advertisement advertisement = getAdvertisementValue();
 
-        loadFragmentAdv(advertisement.getLatitude(), advertisement.getLongitude(), advertisement.getTitulo());
+        loadFragmentAdv(advertisement.getLatitude(), advertisement.getLongitude(),
+                advertisement.getTitulo(), advertisement.getImagem());
 
         this.setTitle(advertisement.getTitulo());
 
@@ -55,11 +56,11 @@ public class AdvertisementActivity extends AppCompatActivity {
         whatsAnuncio.setText(getStringBooleanPrompt(advertisement.getWhatsApp()));
 
 
-        telContato.setOnClickListener(new View.OnClickListener(){
+        telContato.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Uri uri = Uri.parse("tel:"+advertisement.getTelContato());
+                Uri uri = Uri.parse("tel:" + advertisement.getTelContato());
 
                 Intent intent = new Intent(Intent.ACTION_DIAL, uri);
 
@@ -85,17 +86,20 @@ public class AdvertisementActivity extends AppCompatActivity {
                 getString(R.string.prompt_yes) : getString(R.string.prompt_no);
     }
 
-    public void loadFragmentAdv(double latitude, double longitude, String tituloAdv){
+    public void loadFragmentAdv(double latitude, double longitude, String tituloAdv, String imagem) {
+
+        Bundle bundleImg = new Bundle();
+        bundleImg.putString(getString(R.string.key_title_img), imagem);
 
         imgAdvFragment = new ImgAdvFragment();
+        imgAdvFragment.setArguments(bundleImg);
+
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
 
         fragmentTransaction.add(R.id.frag_adv, imgAdvFragment, getString(R.string.TAG_FRAGMENT_IMAGE));
 
-
         Bundle bundle = new Bundle();
-
         bundle.putDouble(getString(R.string.KEY_LATITUDE), latitude);
         bundle.putDouble(getString(R.string.KEY_LONGITUDE), longitude);
         bundle.putString(getString(R.string.key_title_Adv), tituloAdv);
@@ -104,7 +108,7 @@ public class AdvertisementActivity extends AppCompatActivity {
         MapsFragment mapsFrag = new MapsFragment();
         mapsFrag.setArguments(bundle);
 
-        fragmentTransaction.add(R.id.container, mapsFrag, getString(R.string.TAG_FRAGMENT_MAPS) );
+        fragmentTransaction.add(R.id.container, mapsFrag, getString(R.string.TAG_FRAGMENT_MAPS));
 
         fragmentTransaction.commit();
     }
--- app/src/main/java/com/example/valdir/appitarare/ui/fragments/ImgAdvFragment.java
+++ app/src/main/java/com/example/valdir/appitarare/ui/fragments/ImgAdvFragment.java
@@ -1,23 +1,15 @@
 package com.example.valdir.appitarare.ui.fragments;
 
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.example.valdir.appitarare.R;
-import com.example.valdir.appitarare.util.Constants;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -35,38 +27,26 @@ public class ImgAdvFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
+        Bundle bundle = getArguments();
+
+        if (bundle != null) {
+            imgNameAdv = bundle.getString(getString(R.string.key_title_img));
+        }
+
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_img_adv, container, false);
 
-        imgAdver =(ImageView) view.findViewById(R.id.img_fragment_adv);
-
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageReference = storage.getReferenceFromUrl(Constants.URL_IMAGE_STORAGE_FIREBASE).child("bifarma.JPG");
+        imgAdver = (ImageView) view.findViewById(R.id.img_fragment_adv);
 
         //pegar tamanho da tela do celular
-
         DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         final int height = (displayMetrics.heightPixels);
         final int width = (displayMetrics.widthPixels);
 
-        final View viewFragm = view;
+        Picasso.get().load(imgNameAdv).resize(height, width).centerInside().into(imgAdver);
 
-        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-            @Override
-            public void onSuccess(Uri uri) {
-                imgAdver = (ImageView) viewFragm.findViewById(R.id.img_fragment_adv);
-
-                Picasso.get().load(uri.toString()).resize(height, width).centerInside().into(imgAdver);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-            }
-        });
-
-        return  view;
+        return view;
     }
 
 }
